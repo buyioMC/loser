3dba4d9a86e2dbab934841efe0825c31d55712eb
buyioMC <142299025+buyioMC@users.noreply.github.com>
2024-06-16 11:32:17 +0800

    检测设备是否开机
    非常的实用

---
+++ 检测设备是否开机.cpp
@@ -0,0 +1,31 @@
+#include<IOSTREAM>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    cout<<"正在检测设备是否开机，请稍候..."<<endl;
+
+    cout<<"已完成%10"<<endl;
+    Sleep(1000);
+    system("cls");
+
+    cout<<"已完成%20"<<endl;
+    Sleep(1000);
+    system("cls");
+
+
+    cout<<"已完成%60"<<endl;
+    Sleep(1000);
+    system("cls");
+
+    cout<<"已完成%100"<<endl;
+    Sleep(1000);    
+    system("cls");
+
+    cout<<"已检测完成，你的设备已开机！"<<endl;
+
+    system("pause");
+    return 0;
+
+}
